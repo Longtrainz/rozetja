00e5134c641ff23c46b138ad1db3fe4f0f508973
VLAD <chinazaro@gmail.com>
2018-03-24 17:39:04 +0200

    3rd commit

---
+++ src/test/java/rozetkaTest/RegressionTests.java
@@ -0,0 +1,33 @@
+package rozetkaTest;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import rozetkaFramework.HomePage;
+import utils.ConfigProperties;
+
+import java.util.concurrent.TimeUnit;
+
+public class RegressionTests extends TestBase {
+
+
+    @Test(priority = 1)
+    public void openPageTest() {
+        HomePage hp = new HomePage(driver);
+        hp.login("test123321@mail.ru","123QWE");
+    }
+    @Test(priority = 2)
+    public void googleTest() {
+        driver.get("https://www.google.com.ua/");
+        System.out.println("Google");
+    }
+
+    @Test(priority = 3)
+    public void yandexTest() {
+        /// /driver.get("https://yandex.ua/");
+        System.out.println("yandex");
+    }
+}
--- src/test/java/rozetkaTest/Tests.java
+++ src/test/java/rozetkaTest/Tests.java
@@ -9,11 +9,11 @@ public class Tests extends TestBase {
     @Test
     public void loginTest() {
         HomePage hp = new HomePage(driver);
-        hp.login("test123321@mail.ru","123QWE");
+/*        hp.login("test123321@mail.ru","123QWE");
         hp.goToNoteBooksPage();
         hp.filterByPrice();
         hp.getProdWithMaxReviews();
         hp.addToWishList();
-        hp.deleteFromWishList();
+        hp.deleteFromWishList();*/
     }
 }
